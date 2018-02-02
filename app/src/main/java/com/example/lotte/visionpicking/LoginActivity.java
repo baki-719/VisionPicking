ad24f5b41c1acf6a4dd55f588335c6146b15823b
LOTTE <hgs6424@gmail.com>
2018-02-02 17:47:38 +0900

    2018-02-02

--- app/src/main/java/com/example/lotte/visionpicking/LoginActivity.java
+++ app/src/main/java/com/example/lotte/visionpicking/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -13,11 +12,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,34 +25,32 @@ public class LoginActivity extends AppCompatActivity {
 
     public static final String TAG = "LoginActivity";
 
-    //firebase
-    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-    private DatabaseReference mConditionRef = databaseReference.child("Employee");
 
     @BindView(R.id.login_button)
-    Button button;
+    Button loginButton;
+
+    //firebase
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Employee");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
-        Log.d(TAG, "Activity start");
 
     }
 
+
     @OnClick(R.id.login_button)
-    void click(View v) {
-        Log.d(TAG, "clicked");
-        if (v.getId() == R.id.login_button) {
-            if (BasicValue.getInstance().getMode() == 1) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                intent.putExtra("ID", "testID");
-                startActivity(intent);
-            } else {
+    void onClicked() {
+        switch (BasicValue.getInstance().getMode()) {
+            case 0: // QRcode skip
+                break;
+            case 1: // QRcode not skip
                 new IntentIntegrator(this).initiateScan();
-            }
+                break;
         }
+
     }
 
     @Override
@@ -63,33 +59,29 @@ public class LoginActivity extends AppCompatActivity {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (result != null) {
             if (result.getContents() == null) {
-                Toast.makeText(this, "Scanning fail", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "스캔에 실패하였습니다.", Toast.LENGTH_LONG).show();
             } else {
-                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
-                try {
-                    JSONObject object = new JSONObject(result.getContents());
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    intent.putExtra("ID", object.getString("ID"));
-                    startActivity(intent);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
+                JsonParser parser = new JsonParser();
+                JsonObject object = parser.parse(result.getContents()).getAsJsonObject();
+                final Employee loginEmployee = new Employee(object.get("name").getAsString(), object.get("position").getAsString(), object.get("index").getAsLong());
+                Log.d(TAG, loginEmployee.toString());
+                checkId(loginEmployee);
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        mConditionRef.addValueEventListener(new ValueEventListener() {
+    private void checkId(final Employee loginEmployee) {
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String employeeId = dataSnapshot.getValue(String.class);
-                Log.d(TAG, employeeId);
+                for(DataSnapshot tempSnapshot : dataSnapshot.getChildren()) {
+                    Employee temp = tempSnapshot.getValue(Employee.class);
+                    if(loginEmployee.getIndex().equals(temp.getIndex())){
+                        login(loginEmployee);
+                    }
+                }
             }
 
             @Override
@@ -97,6 +89,13 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    private void login(Employee loginEmployee) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.putExtra("OBJECT", loginEmployee);
+        startActivity(intent);
     }
 }
 
